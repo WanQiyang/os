169ac52f36814d66d5d684f4b740abed4c600c45
WanQiyang <qiyangwan@outlook.com>
2018-01-27 11:48:21 +0800

    Add files via upload

---
+++ Project2-Non-Preemptive-Kernel/project2_start_code/task_1/process3.c
@@ -0,0 +1,8 @@
+#include "common.h"
+#include "syslib.h"
+#include "util.h"
+
+void _start(void)
+{
+	/* need student add */
+}
---
+++ Project2-Non-Preemptive-Kernel/project2_start_code/task_1/th3.c
@@ -0,0 +1,11 @@
+#include "common.h"
+#include "scheduler.h"
+#include "util.h"
+
+void thread4(void)
+{
+}
+
+void thread5(void)
+{
+}
